3670d9ee237f6ac4e7e3301d5232bc91c59d282e
Bruno Toniatti <toniattibruno08@gmail.com>
2025-06-11 18:59:15 -0300

    Ajustando carrossel

--- src/app/first-page/components/carousel/carousel.component.ts
+++ src/app/first-page/components/carousel/carousel.component.ts
@@ -18,6 +18,12 @@ export class CarouselComponent {
       description: 'Realce o seu olhar com fios naturais e volume perfeito.',
       whatsMessage: 'Olá! Gostaria de agendar uma extensão de cílios.'
     },
+    {
+      image: 'assets/smile.jpg',
+      title: 'Piercing no Dente',
+      description: 'Brilho no sorriso com aplicação segura e estilosa.',
+      whatsMessage: 'Olá! Tenho interesse em colocar piercing no dente.'
+    }
   ];
 
   currentIndex = 0;
