f8595170574f204f814d9e8557ee58e1bb21c334
Bruno Toniatti <toniattibruno08@gmail.com>
2025-06-16 22:08:12 -0300

    Adicionando para mobile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; // ✅ importa isso
+import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker'; // ✅ importa isso
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient() // ✅ adiciona aqui também
-  ]
+    ,
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
