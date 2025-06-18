0f65ff5e0c7f62eeb8b52d7a69acc47d0ba4263b
Bruno Toniatti <toniattibruno08@gmail.com>
2025-06-17 23:05:36 -0300

    Removendo admin do portifolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { FirstPageComponent } from './first-page/first-page.component';
 import path from 'path';
 import { CalendarPageComponent } from './calendar-page/calendar-page.component';
 import { AppointmentPageComponent } from './appointment-page/appointment-page.component';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
-import { AdminPainelComponent } from './admin-painel/admin-painel.component';
 
 export const routes: Routes = [
   {
@@ -19,12 +17,4 @@ export const routes: Routes = [
     path: 'agendar/:date',
     component: AppointmentPageComponent,
   },
-  {
-    path: 'admin',
-    component: AdminLoginComponent
-  },
-  {
-    path: 'admin/painel',
-    component: AdminPainelComponent
-  }
 ];
