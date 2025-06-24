d5b53e2360eb71669c01b0408039ade5314715ae
Bruno Toniatti <toniattibruno08@gmail.com>
2025-06-24 19:57:24 -0300

    Alterando foto e mensagem

--- src/app/first-page/components/promotion/promotion.component.ts
+++ src/app/first-page/components/promotion/promotion.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class PromotionComponent {
   whatsappLink = 'https://wa.me/5511961006415?text=' +
-    encodeURIComponent('Olá! Gostaria de agendar um piercing dental. #VimPeloPiri');
+    encodeURIComponent('Olá! Gostaria de agendar um piercing dental. *Vim pelo Piri*');
 
   scrollToSection(event: Event, sectionId: string) {
     event.preventDefault();
